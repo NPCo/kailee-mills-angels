55fa1cb821aecd6309cd3166f707fb85e33b894e
Kaelinator <kirkkael@gmail.com>
2018-10-26 14:35:58 -0500

    getting things up to speed

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="angels">
+            <div style={{ background: 'red' }} ></div>
+            <div style={{ background: 'orange' }} ></div>
+            <div style={{ background: 'yellow' }} ></div>
+            <div style={{ background: 'forestgreen' }} ></div>
+            <div style={{ background: 'darkred' }} ></div>
+            <div style={{ background: 'darkorange' }} ></div>
+            <div style={{ background: 'gold' }} ></div>
+            <div style={{ background: 'green' }} ></div>
+            <div style={{ background: 'maroon' }} ></div>
+            <div style={{ background: 'chocolate' }} ></div>
+            <div style={{ background: 'goldenrod' }} ></div>
+            <div style={{ background: 'darkgreen' }} ></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
