5a4e3818ba2a237d5a978f4d5075c2789fa6428a
Kaelinator <kirkkael@gmail.com>
2018-11-09 21:04:34 -0600

    managed gridArea readability

--- src/Angel.js
+++ src/Angel.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react'
 
 export default class Angel extends Component {
 
-    constructor(props) {
-        super(props)
-    }
+	// constructor(props) {
+	// 	super(props)
+	// }
 
-    render() {
-        console.log(this.props.x)
-        return (
-            <div style={{
-                background: this.props.background || `url(${this.props.backgroundImage})`,
-                gridArea: `${this.props.y} / ${this.props.x} / ${this.props.y + (this.props.h || 1)} / ${this.props.x + (this.props.w || 1)}`
-
-            }}></div>
-        )
-    }
+	render() {
+		console.log(this.props.x)
+		return (
+			<div style={{
+				background: this.props.background || `url(${this.props.backgroundImage})`,
+				gridArea: [
+					this.props.y,
+					this.props.x,
+					this.props.y + (this.props.h || 1),
+					this.props.x + (this.props.w || 1)
+				].join(' / ')
+			}}></div>
+		)
+	}
 }
