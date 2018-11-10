7e1b491e500506da37dc208e8cdded3d0f21218b
Kaelinator <kirkkael@gmail.com>
2018-11-09 21:10:48 -0600

    basic scale change on hover

--- src/Angel.js
+++ src/Angel.js
@@ -2,21 +2,44 @@ import React, { Component } from 'react'
 
 export default class Angel extends Component {
 
-	// constructor(props) {
-	// 	super(props)
-	// }
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			hovered: false
+		}
+
+		this.expand = this.expand.bind(this)
+		this.contract = this.contract.bind(this)
+	}
+
+	expand() {
+		this.setState({
+			hovered: true
+		})
+	}
+
+	contract() {
+		this.setState({
+			hovered: false
+		})
+	}
 
 	render() {
 		console.log(this.props.x)
 		return (
-			<div style={{
-				background: this.props.background || `url(${this.props.backgroundImage})`,
-				gridArea: [
-					this.props.y,
-					this.props.x,
-					this.props.y + (this.props.h || 1),
-					this.props.x + (this.props.w || 1)
-				].join(' / ')
+			<div 
+				onMouseEnter={this.expand}
+				onMouseLeave={this.contract}
+				style={{
+					background: this.props.background || `url(${this.props.backgroundImage})`,
+					transform: `scale(${this.state.hovered ? 1.1 : 1.0})`,
+					gridArea: [
+						this.props.y,
+						this.props.x,
+						this.props.y + (this.props.h || 1),
+						this.props.x + (this.props.w || 1)
+					].join(' / ')
 			}}></div>
 		)
 	}
