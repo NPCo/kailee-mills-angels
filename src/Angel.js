d394ccdf68adb8aa338ba3c682aa86e2cc4a0b8d
Kaelinator <kirkkael@gmail.com>
2018-11-19 21:51:29 -0600

    actually getting react transition group to work

--- src/Angel.js
+++ src/Angel.js
@@ -13,6 +13,12 @@ export default class Angel extends Component {
 
 		this.expand = this.expand.bind(this)
 		this.contract = this.contract.bind(this)
+		this.select = this.select.bind(this)
+	}
+
+	componentWillUnmount() {
+		console.log('hello')
+		this.props.trigger()
 	}
 
 	expand() {
@@ -31,25 +37,39 @@ export default class Angel extends Component {
 			})
 	}
 
+	select() {
+		if (!this.enabled)
+			return
+		
+		console.log('flag')
+		this.props.onSelected()
+	}
+
 	render() {
+		const transitionStyles = {
+			exiting: {
+				background: this.props.color
+			},
+		}
+		console.log('this.props.transitionState', this.props.transitionState)
 		return (
 			<div className="angel-element"
 				onMouseEnter={this.expand}
 				onMouseLeave={this.contract}
-				onClick={this.enabled && this.props.onSelected}
+				onClick={this.select}
 				style={{
 					cursor: (this.enabled) ? 'pointer' : 'default',
 					background: `url(${this.props.thumbnail}) no-repeat 50% 50%`,
 					backgroundColor: this.props.color,
 					transform: `scale(${this.state.hovered ? 1.1 : 1.0})`,
 					zIndex: this.state.zIndex,
-					position: (this.state.revealed) ? 'relative' : 'static',
 					gridArea: [
 						this.props.y,
 						this.props.x,
 						this.props.y + (this.props.h || 1),
 						this.props.x + (this.props.w || 1)
-					].join(' / ')
+					].join(' / '),
+					...transitionStyles[this.props.transitionState]
 			}}></div>
 		)
 	}
--- src/AngelGrid.js
+++ src/AngelGrid.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react'
+import TransitionGroup from 'react-transition-group/TransitionGroup'
+import Transition from 'react-transition-group/Transition'
+import sizeMe from 'react-sizeme'
+import uuid from 'uuid'
 
 import Angel from './Angel.js'
+
+const HEIGHT_GAP = 10
 
 export default class AngelGrid extends Component {
 
   state = {
-    selectedIndex: -1
+    selectedId: null,
+    triggered: false
   }
 
-  selectIndex(i) {
+  constructor(props) {
+    super(props)
+
+    this.angels = this.props.angels
+      .map(a => Object.assign({ id: uuid() }, a))
+
+    this.selectId = this.selectId.bind(this)
+    this.trigger = this.trigger.bind(this)
+  }
+
+  selectId(id) {
+    console.log(id)
     this.setState({
-      selectedIndex: i
+      selectedId: id,
+      triggered: false
+    })
+  }
+
+  trigger() {
+    this.setState({
+      triggered: true
     })
   }
 
@@ -18,24 +43,33 @@ export default class AngelGrid extends Component {
 
     const angel = Object.assign(
       { color: 'white', photo: '#', name: 'An error has occured', dates: '', bio: [] },
-      this.props.angels[this.state.selectedIndex]
+      this.angels.find(a => a.id === this.state.selectedId)
     )
 
-    return (this.state.selectedIndex === -1)
+    return (!this.state.triggered)
       ? (
         <div className="angels" style={{
           display: 'grid',
           gridTemplateColumns: `repeat(${this.props.columns || 4}, ${this.props.width || 100}px)`,
           gridTemplateRows: `repeat(${this.props.rows || 3}, ${this.props.height || 100}px)`,
           columnGap: `calc((100% - ${this.props.columns * this.props.width}px) / ${this.props.columns - 1})`,
-          height: `${this.props.rows * (this.props.height + 10) - 10}px`,
-          rowGap: '10px'
+          height: `${this.props.rows * (this.props.height + HEIGHT_GAP) - HEIGHT_GAP}px`,
+          rowGap: `${HEIGHT_GAP}px`
         }}>
-          {
-            this.props.angels
-              .map((a, i) => 
-              <Angel key={i} onSelected={() => this.selectIndex(i)} {...a} />)
-          }
+          <TransitionGroup component={null}>
+            {
+              this.angels
+                .filter(a => a.id !== this.state.selectedId)
+                .map((a) => (
+			            <Transition key={`angel-${a.id}`} timeout={1000} unmountOnExit>
+                    {state => 
+                    <Angel transitionState={state}
+                    onSelected={() => this.selectId(a.id)}
+                    trigger={() => this.trigger()} {...a} />}
+                  </Transition>
+                ))
+            }
+          </TransitionGroup>
         </div>
       )
       : (
@@ -50,7 +84,7 @@ export default class AngelGrid extends Component {
               <div className="angel-title">
                 <span className="angel-name">{angel.name}</span>
                 <span className="angel-dates">{angel.dates}</span>
-                <div className="angel-exit" onClick={() => this.selectIndex(-1)}><div className="close icon"></div></div>
+                <div className="angel-exit" onClick={() => this.selectId(null)}><div className="close icon"></div></div>
               </div>
               <div className="angel-bio">
                 {angel.bio.map((text, i) => <p key={i}>{text}</p>)}
