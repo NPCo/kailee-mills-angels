3397c52de0242df01fc303ba2e349876890c1560
Kaelinator <kirkkael@gmail.com>
2018-12-04 00:16:55 -0600

    fixed awkward margins for expanded angel

--- src/components/AngelExpanded.js
+++ src/components/AngelExpanded.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 export default ({ angel, height, exit }) => (
   <div style={{ backgroundColor: angel.color, height }} className="angel-expanded">
-    <div className="fade-in">
-      <div className="angel-photo">
+    {/* <div > */}
+      <div className="angel-photo fade-in">
         <img alt={angel.name} src={angel.photo} />
       </div>
-      <div className="angel-title">
+      <div className="angel-title fade-in">
         <span className="angel-name">{angel.name}</span>
         <span className="angel-dates">{angel.dates}</span>
         <div className="angel-exit" onClick={exit}><div className="close icon"></div></div>
       </div>
-      <div className="angel-bio">
+      <div className="angel-bio fade-in">
         {angel.bio.map((text, i) => <p key={i}>{text}</p>)}
       </div>
-    </div>
+    {/* </div> */}
   </div>
 )
