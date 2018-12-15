c6f62f060ba2f2998f78dfa86413a48150474c92
Kaelinator <kirkkael@gmail.com>
2018-12-15 17:36:54 -0600

    stateless form

--- src/components/AngelEditForm.js
+++ src/components/AngelEditForm.js
@@ -1,62 +1,50 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Form, Text, TextArea } from 'informed'
 
-export default class AngelEditForm extends Component {
+export default props => (
+  <div style={{ backgroundColor: props.color }}>
+    <Form
+      key={props._id}
+      initialValues={props}
+      onValueChange={formState => console.log('edited', formState)}
+      onSubmit={formState => console.log('submitted', formState)}>
+      <div className="edit-angel-form">
+        <div className="angel-form-info">
+          <label htmlFor="angel-name">Name:</label>
+          <Text field="name" id="angel-name" />
 
+          <label htmlFor="angel-dates">Dates:</label>
+          <Text field="dates" id="angel-dates" placeholder="e.g. 2000 - 2000" />
 
-  constructor(props) {
-    super(props)
+          <label htmlFor="angel-color">Color:</label>
+          <Text field="color" id="angel-color" placeholder="e.g. #d7f5e1" />
 
-    this.state = {}
-  }
+          <label htmlFor="angel-photo">Biography Photo:</label>
+          <Text field="photo" id="angel-photo" placeholder="e.g. https://static.wixstatic.com/media/img.jpg" />
 
-  render() {
-    return (
-      <div style={{ backgroundColor: this.props.color }}>
-        <Form
-          key={this.props._id}
-          initialValues={this.props}
-          onValueChange={formState => console.log('edited', formState)}
-          onSubmit={formState => console.log('submitted', formState)}>
-          <div className="edit-angel-form">
-            <div className="angel-form-info">
-              <label htmlFor="angel-name">Name:</label>
-              <Text field="name" id="angel-name" />
+          <label htmlFor="angel-thumbnail">Thumbnail Photo:</label>
+          <Text field="thumbnail" id="angel-thumbnail" placeholder="e.g. https://static.wixstatic.com/media/img.jpg" />
+        </div>
+        <div className="angel-form-bio">
+          <label htmlFor="angel-bio">Biography:</label>
+          <TextArea field="bio" id="angel-bio" />
+        </div>
+        <div className="angel-form-info">
+          <label htmlFor="angel-col">Column:</label>
+          <Text field="x" id="angel-col" />
 
-              <label htmlFor="angel-dates">Dates:</label>
-              <Text field="dates" id="angel-dates" placeholder="e.g. 2000 - 2000" />
+          <label htmlFor="angel-row">Row:</label>
+          <Text field="y" id="angel-row" />
 
-              <label htmlFor="angel-color">Color:</label>
-              <Text field="color" id="angel-color" placeholder="e.g. #d7f5e1" />
+          <label htmlFor="angel-width">Width:</label>
+          <Text field="w" id="angel-width" />
 
-              <label htmlFor="angel-photo">Biography Photo:</label>
-              <Text field="photo" id="angel-photo" placeholder="e.g. https://static.wixstatic.com/media/img.jpg" />
+          <label htmlFor="angel-height">Height:</label>
+          <Text field="h" id="angel-height" />
 
-              <label htmlFor="angel-thumbnail">Thumbnail Photo:</label>
-              <Text field="thumbnail" id="angel-thumbnail" placeholder="e.g. https://static.wixstatic.com/media/img.jpg" />
-            </div>
-            <div className="angel-form-bio">
-              <label htmlFor="angel-bio">Biography:</label>
-              <TextArea field="bio" id="angel-bio" />
-            </div>
-            <div className="angel-form-info">
-              <label htmlFor="angel-col">Column:</label>
-              <Text field="x" id="angel-col" />
-
-              <label htmlFor="angel-row">Row:</label>
-              <Text field="y" id="angel-row" />
-
-              <label htmlFor="angel-width">Width:</label>
-              <Text field="w" id="angel-width" />
-
-              <label htmlFor="angel-height">Height:</label>
-              <Text field="h" id="angel-height" />
-
-              <button type="submit" style={{ gridColumn: '1 / 3' }}>Submit</button>
-            </div>
-          </div>
-        </Form>
+          <button type="submit" style={{ gridColumn: '1 / 3' }}>Submit</button>
+        </div>
       </div>
-    )
-  }
-}
+    </Form>
+  </div>
+)
