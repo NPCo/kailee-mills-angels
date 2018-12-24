60161380dbc4df591f4feba4201a0cc09231e60a
Kaelinator <kirkkael@gmail.com>
2018-12-24 11:30:41 -0600

    stories of AngelListItem

--- client/src/stories/index.js
+++ client/src/stories/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-// import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+// import { linkTo } from '@storybook/addon-links'
+
+import '../index.css'
 
 import AngelListItem from '../components/AngelListItem'
 
 storiesOf('AngelListItem', module)
-  .add('first example', () => <AngelListItem color="#555" name="Me" onClick={action('clicking')} />)
+  .add('named', () => <AngelListItem color="#deeeee" name="Me" onClick={action('clicked')} />)
+  .add('unnamed', () => <AngelListItem color="#deeeee" onClick={action('clicked')} />)
+  .add('uncolored', () => <AngelListItem name="Me" onClick={action('clicked')} />)
