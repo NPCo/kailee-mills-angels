6582136b3e73086285ffdc34d756d6f23d7a5594
Kaelinator <kirkkael@gmail.com>
2018-12-24 19:26:48 -0600

    stories of EditableAngelGrid

--- client/src/stories/index.js
+++ client/src/stories/index.js
@@ -8,13 +8,13 @@ import '../index.css'
 
 import AngelListItem from '../components/AngelListItem'
 import AngelDisplay from '../views/AngelDisplay'
+import EditableAngelGrid from '../views/EditableAngelGrid'
 
 storiesOf('AngelListItem', module)
   .add('named', () => <AngelListItem color="#deeeee" name="Me" onClick={action('clicked')} />)
   .add('unnamed', () => <AngelListItem color="#deeeee" onClick={action('clicked')} />)
   .add('uncolored', () => <AngelListItem name="Me" onClick={action('clicked')} />)
 
-{
 const angelData = [
   { color: '#deeeee', name: 'Me', bio: 'lorem ipsum', dates: '2001 - 2018', photo: 'https://via.placeholder.com/140x100', thumbnail: 'https://via.placeholder.com/140x100', x: 1, y: 1, w: 1, h: 1 },
   { color: '#d7f5e1', name: 'Me', bio: 'lorem ipsum', dates: '2001 - 2018', photo: 'https://via.placeholder.com/140x100', thumbnail: 'https://via.placeholder.com/140x100', x: 1, y: 2, w: 2, h: 1 },
@@ -27,4 +27,6 @@ const dimensions = { width: 250, height: 150 }
 storiesOf('AngelDisplay', module)
   .add('desktop', () => <div style={{ width: '1000px' }}><AngelDisplay angels={angelData} {...dimensions} /></div>)
   .add('mobile', () => <div style={{ width: '360px' }}><AngelDisplay angels={angelData} {...dimensions} /></div>)
-}
+
+storiesOf('EditableAngelGrid', module)
+  .add('desktop', () => <div style={{ width: '1000px' }}><EditableAngelGrid angels={angelData} /></div>)
